4f8d49d8799840c23be3c7dc6643cf7e94749f4f
Joshua Vasquez <jvzga@Joshuas-MacBook-Air.local>
2026-03-26 08:52:25 -0400

    Finished canvas 1 practice

--- practice/loops/loops.js
+++ practice/loops/loops.js
@@ -1,7 +1,11 @@
 let canvas = document.getElementById("canvas1");
 let ctx = canvas.getContext("2d");
 
-//fill in code for canvas1 here
+for(let y = 50; y <= 150; y+=50){
+    for(let x = 0; x <=800; x +=75){
+        ctx.fillRect(x, y, 50, 20);
+    }
+}
 
 canvas = document.getElementById("canvas2");
 ctx = canvas.getContext("2d");

6041cf8300584631d6d204dcef4300f148b3ac6b
Joshua Vasquez <jvzga@Joshuas-MacBook-Air.local>
2026-03-26 08:35:43 -0400

    Added loop function practice files

---
+++ practice/loops/loops.js
@@ -0,0 +1,19 @@
+let canvas = document.getElementById("canvas1");
+let ctx = canvas.getContext("2d");
+
+//fill in code for canvas1 here
+
+canvas = document.getElementById("canvas2");
+ctx = canvas.getContext("2d");
+
+//fill in code for canvas2 here
+
+canvas = document.getElementById("canvas3");
+ctx = canvas.getContext("2d");
+
+//fill in code for canvas3 here
+
+canvas = document.getElementById("canvas4");
+ctx = canvas.getContext("2d");
+
+//fill in code for canvas4 here

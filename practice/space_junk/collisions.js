4084241bf99c31b2c3abd62eed7f4bbc2d7df597
Joshua Vasquez <jvzga@Joshuas-MacBook-Air.local>
2026-04-08 09:18:16 -0400

    Added collisons and click item features

--- practice/space_junk/collisions.js
+++ practice/space_junk/collisions.js
@@ -9,6 +9,13 @@ function clickHandler(e){
     //TODO: write some code here that checks whether
     //(mouseX, mouseY) is inside j
 
+    if (mouseX >= junk_min_x 
+        && mouseX <= junk_max_x 
+        && mouseY >= junk_min_y 
+        && mouseY <= junk_max_y){
+        j.alive = false;
+    }
+
   }
 }
 
@@ -30,6 +37,14 @@ function checkCollisions(){
                 ship_max_x = s.x + 15*s.size;
                 ship_min_y = s.y - 15*s.size;
                 ship_max_y = s.y + 15*s.size;
+
+                if (junk_max_x >= ship_min_x
+                    && junk_min_x <= ship_max_x
+                    && junk_max_y >= ship_min_y
+                    && junk_min_y <= ship_max_y){
+                        j.alive = false;
+                        s.alive = false;
+                }
 
             }
         }
